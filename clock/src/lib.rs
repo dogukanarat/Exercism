700f1d992f0080d975753e59c118169940f8212e
Dogukan Arat <aratdogukan@gmail.com>
2022-06-11 15:49:21 +0300

    Solve the problem completely

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -3,39 +3,54 @@ use std::fmt;
 #[derive(Debug, PartialEq)]
 pub struct Clock
 {
-    time_in_seconds: i64,
+    time_in_minutes: i64,
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let hours = (self.time_in_seconds / 3600) % 24;
-        let minutes = ((self.time_in_seconds % 3600) / 60) % 60;
+        let hours = (self.time_in_minutes / 60) % 24;
+        let minutes = self.time_in_minutes  % 60;
         write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
 
 impl Clock {
-    pub fn new(mut hours: i32, mut minutes: i32) -> Self {
-        let mut time_in_seconds = 0;
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let mut time_in_minutes = 0;
 
-        minutes = if minutes < 0 { hours = hours - 1; minutes % 60 + 60 } else { minutes };
-        hours = if hours < 0 { hours % 24 + 24 } else { hours };
+        time_in_minutes += hours as i64 * 60;
+        time_in_minutes += minutes as i64;
 
+        if time_in_minutes > 0
+        {
+            time_in_minutes %= 1440;
+        }
+        else
+        {
+            time_in_minutes = 1440 + time_in_minutes % 1440;
+        }
 
-        time_in_seconds += hours as i64 * 60 * 60;
-        time_in_seconds += minutes as i64 * 60;
-        time_in_seconds = time_in_seconds % (24 * 60 * 60);
-        Clock { time_in_seconds }
+        time_in_minutes = time_in_minutes % (24 * 60);
+
+        Clock { time_in_minutes }
     }
 
-    pub fn add_minutes(&self, mut minutes: i32) -> Self {
-        let mut time_in_seconds = self.time_in_seconds;
-        let mut hours = (time_in_seconds / 3600) % 24;
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        let mut time_in_minutes = self.time_in_minutes;
 
-        minutes = if minutes < 0 { hours = hours - 1; minutes % 60 + 60 } else { minutes };
+        time_in_minutes += minutes as i64;
 
-        time_in_seconds += minutes as i64 * 60;
-        time_in_seconds = time_in_seconds % (24 * 60 * 60);
-        Clock { time_in_seconds }
+        if time_in_minutes > 0
+        {
+            time_in_minutes %= 1440;
+        }
+        else
+        {
+            time_in_minutes = 1440 + time_in_minutes % 1440;
+        }
+
+        time_in_minutes = time_in_minutes % (24 * 60);
+
+        Clock { time_in_minutes }
     }
 }
